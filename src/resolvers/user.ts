120bc86512c5d08528d0ed2631f9bed287f7a1c8
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2020-09-17 20:03:23 -0300

    Expire token after change password

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -47,7 +47,8 @@ export class UserResolver {
         ],
       };
     }
-    const userId = await redis.get(FORGET_PASSWORD_PREFIX + token);
+    const key = FORGET_PASSWORD_PREFIX + token;
+    const userId = await redis.get(key);
     if (!userId) {
       return {
         errors: [{ field: 'token', message: 'token expired' }],
@@ -65,7 +66,7 @@ export class UserResolver {
     user.password = await argon2.hash(newPassword);
 
     await em.persistAndFlush(user);
-
+    await redis.del(key);
     req.session.userId = user.id;
 
     return { user };
